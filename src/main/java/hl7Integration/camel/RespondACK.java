3255518e469f80069258a08054d3eb70657d29c1
ignacio suay <ignacio.suay@allocatesoftware.com>
2013-11-08 09:45:45 +0000

    Use Spring annotations

--- src/main/java/hl7Integration/camel/RespondACK.java
+++ src/main/java/hl7Integration/camel/RespondACK.java
@@ -1,7 +1,9 @@
 package hl7Integration.camel;
 
 import ca.uhn.hl7v2.model.Message;
+import org.springframework.stereotype.Component;
 
+@Component
 public class RespondACK {
 
     public Message process(Message in) throws Exception {
